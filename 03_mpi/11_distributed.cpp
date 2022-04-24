af4046b4c1d0597d982c2f404427d194a9e1baef
bokkuru <kamisamasugoi3@gmail.com>
2022-04-25 06:08:38 +0900

    modify 03-11

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
     //MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
     //MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     MPI_Win win;
-    MPI_WIN_create(jbody, N/size*sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    MPI_Win_create(jbody, N/size*sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
     MPI_Win_fence(0, win);
     MPI_Put(jbody, N/size, MPI_BODY, send_to, 0, N/size, MPI_BODY, win);
     MPI_Win_fence(0, win);
