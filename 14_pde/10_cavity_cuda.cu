dd22ff5131ac117736d206421b8a2bb32d157483
bokkuru <kamisamasugoi3@gmail.com>
2022-06-13 05:32:47 +0900

    final cuda

--- 14_pde/10_cavity_cuda.cu
+++ 14_pde/10_cavity_cuda.cu
@@ -24,14 +24,12 @@ __global__ void cavity(double *u,double *v,double *b,double *p,double *un,double
                         ((u[j+(i+1)*ny] - u[j+(i-1)*ny])/(2*dx)+(v[j+1+i*ny]-v[j-1+i*ny])/(2*dy))-
                         pow2((u[j+(i+1)*ny]-u[j+(i-1)*ny])/(2*dx)) - 2 *((u[j+1+i*ny]-u[j-1+i*ny])/(2*dy)*
                         (v[j+(i+1)*ny]-v[j+(i-1)*ny])/(2*dx))-pow2((v[j+1+i*ny]-v[j-1+i*ny])/(2*dy)));
-        __syncthreads();
         for (int it = 0; it < nit; it++){
             pn = p;
             p[j+i*ny] = (dy * dy * (pn[j+(i+1)*ny] + pn[j+(i-1)*ny]) +
                         dx * dx * (pn[j+1+i*ny] + pn[j-1+i*ny]) -
                         b[j+i*ny]  * dx * dx * dy * dy)
                         /(2 * (dx * dy + dy * dy));
-            __syncthreads();
             p[ny - 1+i*ny] = 0;
             p[i*ny] = p[1+i*ny];
             p[i+ny*(nx - 1)] = p[i+ny*(nx - 2)];
@@ -39,7 +37,6 @@ __global__ void cavity(double *u,double *v,double *b,double *p,double *un,double
         }
         un = u;
         vn = v;
-        __syncthreads();
         u[j+i*ny] = un[j+i*ny] - un[j+i*ny] * dt / dx * (un[j+i*ny] - un[j+(i-1)*ny])
                     - un[j+i*ny] * dt / dy * (un[j+i*ny] - un[j-1+i*ny])
                     - dt / (2 * rho * dx) * (p[j+(i+1)*ny] - p[j+(i-1)*ny])
@@ -50,7 +47,6 @@ __global__ void cavity(double *u,double *v,double *b,double *p,double *un,double
                     - dt / (2 * rho * dx) * (p[j+1+i*ny] - p[j-1+i*ny])
                     + nu * dt / (dx * dx) * (vn[j+(i+1)*ny] - 2 * vn[j+i*ny] + vn[j+(i-1)*ny])
                     + nu * dt / (dy * dy) * (vn[j+1+i*ny] - 2 * vn[j+i*ny] + vn[j-1+i*ny]);
-        __syncthreads();
         u[j] = 0;
         u[j+ny*(nx - 1)] = 0;
         v[j] = 0;
